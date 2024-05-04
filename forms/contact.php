9701705e8661b886a14ab702102bc2d788168995
Reema <yadav.ree@northeastern.edu>
2024-05-04 14:39:04 -0700

    Portfolio
    Deleted the old folder that was not required. Added pinterest link.

--- forms/contact.php
+++ forms/contact.php
@@ -6,8 +6,8 @@
   * For more info and help: https://bootstrapmade.com/php-email-form/
   */
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  // receiving email address
+  $receiving_email_address = 'reemayadav1702@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
